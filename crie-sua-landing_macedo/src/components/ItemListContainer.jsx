17ad2211db2fb040de46fa00d09f742780ba70b6
Camila Oliveira Macedo <cam.omacedo@outlook.com>
2025-06-20 18:48:25 -0300

    primeira_entrega
    primeira_entrega

---
+++ crie-sua-landing_macedo/src/components/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ItemListContainer = ({ greeting }) => {
+  return (
+    <section>
+      <h2>{greeting}</h2>
+      {/* Catálogo de produtos vai aqui futuramente */}
+    </section>
+  );
+};
+
+export default ItemListContainer;
